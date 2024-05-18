03229cd90017790ec19135b5a4f966a4c2ec5325
lucaslov <czyzukasz2402@gmail.com>
2024-05-18 14:06:57 +0200

    Update the model

--- PokemonRecognition/main.py
+++ PokemonRecognition/main.py
@@ -1,200 +1,182 @@
 import argparse
 import logging
 import os
-import time
-import pandas as pd
+from typing import Tuple, List, Dict
+
 import torch
-import matplotlib.pyplot as plt
+import pandas as pd
 from torch.nn import Module, Conv2d, MaxPool2d, Linear, ReLU, LogSoftmax
-from torch import flatten
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from torch.optim import Adam
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 
-#==================================================================================================
-#                                            LOGGER
-#==================================================================================================
-
 logger = logging.getLogger()
-lprint = logger.info
+logger_formatter = logging.Formatter('%(message)s')
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(logger_formatter)
+logger.addHandler(console_handler)
+logger.setLevel(logging.INFO)
 
-def setup_logger()-> None:
-    log_formatter = logging.Formatter('%(message)s')
-    logfile_path = os.path.join(os.getcwd(), 'dataset.log')
-    file_handler = logging.FileHandler(logfile_path)
-    file_handler.setFormatter(log_formatter)
-    logger.addHandler(file_handler)
+class LeNet(Module):
+    """
+    LeNet neural network model.
+    """
+    def __init__(self, input_shape: torch.Size, num_classes: int) -> None:
+        super().__init__()
+        num_channels = input_shape[0]
 
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(log_formatter)
-    logger.addHandler(console_handler)
+        self.conv1 = Conv2d(in_channels=num_channels, out_channels=20, kernel_size=(5, 5))
+        self.relu1 = ReLU()
+        self.maxpool1 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
 
-    logger.setLevel(logging.INFO)
+        conv_out = int((input_shape[1] - 5) / 2) + 1
 
-def print_text_separator():
-    lprint('--------------------------------------------------------')
+        self.conv2 = Conv2d(in_channels=20, out_channels=50, kernel_size=(5, 5))
+        self.relu2 = ReLU()
+        self.maxpool2 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
 
-#==================================================================================================
-#                                         NEURAL NETWORKS
-#==================================================================================================
+        conv_out = int((conv_out - 5) / 2) + 1
+        conv_size = conv_out * conv_out * 50
+        logger.info(conv_out)
 
-class PokeNet(Module):
-    def __init__(self, input_shape: torch.Size, classes: int):
-        super(PokeNet, self).__init__()
-        channel_count = input_shape[0]
-        
-        self.features = torch.nn.Sequential(
-            Conv2d(channel_count, 64, kernel_size=3, padding=1), ReLU(),
-            Conv2d(64, 64, kernel_size=3, padding=1), ReLU(),
-            MaxPool2d(kernel_size=2, stride=2),
-            
-            Conv2d(64, 128, kernel_size=3, padding=1), ReLU(),
-            Conv2d(128, 128, kernel_size=3, padding=1), ReLU(),
-            MaxPool2d(kernel_size=2, stride=2),
-            
-            Conv2d(128, 256, kernel_size=3, padding=1), ReLU(),
-            Conv2d(256, 256, kernel_size=3, padding=1), ReLU(),
-            MaxPool2d(kernel_size=2, stride=2),
-        )
-        
-        def calc_conv_out(input_size, layers):
-            size = input_size
-            for layer in layers:
-                if isinstance(layer, Conv2d):
-                    size = (size - layer.kernel_size[0] + 2*layer.padding[0]) // layer.stride[0] + 1
-                elif isinstance(layer, MaxPool2d):
-                    size = (size - layer.kernel_size) // layer.stride + 1
-            return size
-        
-        conv_height = calc_conv_out(input_shape[1], self.features)
-        conv_width = calc_conv_out(input_shape[2], self.features)
-        conv_size = conv_height * conv_width * 256
-        
-        self.classifier = torch.nn.Sequential(
-            Linear(conv_size, 1024), ReLU(),
-            Linear(1024, 512), ReLU(),
-            Linear(512, classes),
-            LogSoftmax(dim=1)
-        )
+        self.fc1 = Linear(in_features=conv_size, out_features=500)
+        self.relu3 = ReLU()
+        self.fc2 = Linear(in_features=500, out_features=num_classes)
+        self.log_softmax = LogSoftmax(dim=1)
 
-    def forward(self, x):
-        x = self.features(x)
-        x = flatten(x, 1)
-        x = self.classifier(x)
-        return x
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass of the model.
+        """
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.maxpool1(x)
 
-#==================================================================================================
-#                                         TRAINING WRAPPER 
-#==================================================================================================
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.maxpool2(x)
 
-def parse_arguments():
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        x = self.relu3(x)
+        x = self.fc2(x)
+        return self.log_softmax(x)
+
+def parse_args() -> argparse.Namespace:
+    """
+    Parse command-line arguments.
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument('-tr', '--train_path', type=str, required=True, help="Path to the training dataset")
-    parser.add_argument('-te', '--test_path', type=str, required=True, help="Path to the testing dataset")
+    parser.add_argument('-d', '--dataset_path', type=str, required=True)
+    parser.add_argument('-o', '--output_path', type=str, required=True)
     parser.add_argument('-b', '--batch_size', type=int, default=32)
+    parser.add_argument('-t', '--train_split', type=float, default=0.7)
     parser.add_argument('--initial_learning_rate', type=float, default=1e-3)
-    parser.add_argument('--epochs', type=int, default=3)
+    parser.add_argument('--epochs', type=int, default=10)
     return parser.parse_args()
 
-
-def get_data_loaders(train_path: str, test_path: str, batch_size: int):
+def get_data_loaders(
+    dataset_path: str, train_split: float, batch_size: int
+) -> Tuple[DataLoader, DataLoader, List[str]]:
+    """
+    Get data loaders for training and validation datasets.
+    """
     transform = transforms.Compose([transforms.ToTensor()])
-    train_dataset = ImageFolder(train_path, transform=transform)
-    test_dataset = ImageFolder(test_path, transform=transform)
+    img_folder = ImageFolder(dataset_path, transform=transform)
+
+    num_train_samples = int(len(img_folder) * train_split)
+    num_val_samples = len(img_folder) - num_train_samples
+
+    train_data, val_data = random_split(
+        img_folder, [num_train_samples, num_val_samples],
+        generator=torch.Generator().manual_seed(42)
+    )
     
-    train_loader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size)
+    train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size)
+    val_loader = DataLoader(val_data, batch_size=batch_size)
+        
+    return train_loader, val_loader, img_folder.classes
+
+def train_model(
+    learning_rate: float, epochs: int, train_loader: DataLoader,
+    val_loader: DataLoader, class_names: List[str]
+) -> Tuple[LeNet, Dict[str, List[float]]]:
+    """
+    Train the LeNet model.
+    """
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    logger.info('Device -> %s', device)
+
+    model = LeNet(next(iter(train_loader))[0][0].shape, len(class_names)).to(device)
+    optimizer = Adam(model.parameters(), lr=learning_rate)
+    loss_function = torch.nn.NLLLoss()
+
+    history: Dict[str, List[float]] = {"train_loss": [], "train_acc": [], "val_loss": [], "val_acc": []}
     
-    return train_loader, test_loader, train_dataset.classes
-
-
-def train_network(initial_learning_rate: float, epochs: int,
-                  train_loader: DataLoader, test_loader: DataLoader,
-                  classes: list[str]):
-    # device_name = "cuda" if torch.cuda.is_available() else "cpu"
-    device_name="mps"
-    lprint(f'Device -> {device_name}')
-    device = torch.device(device_name)
-    input_shape = next(iter(train_loader))[0][0].shape
-    model = PokeNet(input_shape, len(classes)).to(device)
-    opt = Adam(model.parameters(), lr=initial_learning_rate)
-    lossFn = torch.nn.NLLLoss()
-    lprint('Initializing training')
-    history = {
-        "train_loss": [],
-        "train_acc": [],
-        "test_loss": [],
-        "test_acc": []
-    }
-
-    print("[INFO] training the network...")
-    startTime = time.time()
-    for e in range(0, epochs):
+    for epoch in range(epochs):
         model.train()
-        totalTrainLoss = 0
-        totalTestLoss = 0
-        trainCorrect = 0
-        testCorrect = 0
-        for (x, y) in train_loader:
-            (x, y) = (x.to(device), y.to(device))
-            pred = model(x)
-            loss = lossFn(pred, y)
-            opt.zero_grad()
+        total_train_loss, total_val_loss = 0.0, 0.0
+        train_correct, val_correct = 0, 0
+
+        for images, labels in train_loader:
+            images, labels = images.to(device), labels.to(device)
+            optimizer.zero_grad()
+            predictions = model(images)
+            loss = loss_function(predictions, labels)
             loss.backward()
-            opt.step()
-            totalTrainLoss += loss
-            trainCorrect += (pred.argmax(1) == y).type(
-                torch.float).sum().item()
+            optimizer.step()
+            total_train_loss += loss.item()
+            train_correct += (predictions.argmax(1) == labels).type(torch.float).sum().item()
 
+        model.eval()
         with torch.no_grad():
-            model.eval()
-            for (x, y) in test_loader:
-                (x, y) = (x.to(device), y.to(device))
-                pred = model(x)
-                totalTestLoss += lossFn(pred, y)
-                testCorrect += (pred.argmax(1) == y).type(
-                    torch.float).sum().item()
-                
-        avgTrainLoss = totalTrainLoss / len(train_loader)
-        avgTestLoss = totalTestLoss / len(test_loader)
-        trainCorrect = trainCorrect / len(train_loader.dataset)
-        testCorrect = testCorrect / len(test_loader.dataset)
-        history["train_loss"].append(avgTrainLoss.cpu().detach().numpy())
-        history["train_acc"].append(trainCorrect)
-        history["test_loss"].append(avgTestLoss.cpu().detach().numpy())
-        history["test_acc"].append(testCorrect)
-        print("[INFO] EPOCH: {}/{}".format(e + 1, epochs))
-        print("Train loss: {:.6f}, Train accuracy: {:.4f}".format(
-            avgTrainLoss, trainCorrect))
-        print("Test loss: {:.6f}, Test accuracy: {:.4f}\n".format(
-            avgTestLoss, testCorrect))
+            for images, labels in val_loader:
+                images, labels = images.to(device), labels.to(device)
+                predictions = model(images)
+                total_val_loss += loss_function(predictions, labels).item()
+                val_correct += (predictions.argmax(1) == labels).type(torch.float).sum().item()
 
-    endTime = time.time()
-    print("[INFO] total time taken to train the model: {:.2f}s".format(
-        endTime - startTime))
-    
-    script_directory = os.getcwd()
-    model_save_path = os.path.join(script_directory, 'pokemon_model.pth')
-    torch.save(model.state_dict(), model_save_path)
-    history_df = pd.DataFrame(history)
-    history_save_path = os.path.join(script_directory, 'training_history.csv')
-    history_df.to_csv(history_save_path, index=False)
+        avg_train_loss = total_train_loss / len(train_loader)
+        avg_val_loss = total_val_loss / len(val_loader)
+        train_accuracy = train_correct / len(train_loader.dataset)
+        val_accuracy = val_correct / len(val_loader.dataset)
+
+        history["train_loss"].append(avg_train_loss)
+        history["train_acc"].append(train_accuracy)
+        history["val_loss"].append(avg_val_loss)
+        history["val_acc"].append(val_accuracy)
+
+        logger.info("EPOCH: %d/%d", epoch + 1, epochs)
+        logger.info("Train loss: %.6f, Train accuracy: %.4f", avg_train_loss, train_accuracy)
+        logger.info("Val loss: %.6f, Val accuracy: %.4f\n", avg_val_loss, val_accuracy)
 
     return model, history
 
-def visualisation_of_history(history):
-    plt.title('Accuracy')
-    plt.plot(history['train_acc'], '-', label='Train')
-    plt.plot(history['test_acc'], '--', label='Test')
-    plt.legend()
-    plt.show()
+def save_model_and_history(
+    model: LeNet, history: Dict[str, List[float]], output_path: str
+) -> None:
+    """
+    Save the trained model and training history to files.
+    """
+    os.makedirs(output_path, exist_ok=True)
+    torch.save(model.state_dict(), os.path.join(output_path, 'pokemon_model.pth'))
 
-def main(args):
-    setup_logger()
-    train_loader, test_loader, classes = get_data_loaders(args.train_path, args.test_path, args.batch_size)
-    model, history = train_network(args.initial_learning_rate, args.epochs, train_loader, test_loader, classes)
-    visualisation_of_history(history)
+    history_df = pd.DataFrame(history)
+    history_df.to_csv(os.path.join(output_path, 'training_history.csv'), index=False)
+
+def main() -> None:
+    """
+    Main function to execute the training process.
+    """
+    args = parse_args()
+    train_loader, val_loader, class_names = get_data_loaders(
+        args.dataset_path, args.train_split, args.batch_size
+    )
+    model, history = train_model(
+        args.initial_learning_rate, args.epochs, train_loader, val_loader, class_names
+    )
+    save_model_and_history(model, history, args.output_path)
 
 if __name__ == '__main__':
-    main(parse_arguments())
+    main()
